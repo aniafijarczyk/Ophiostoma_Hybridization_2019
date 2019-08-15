f2d106d69cf6bb4f20874fe170c9dd71e2477fc5
Anna Fijarczyk <aniafijarczyk@gmail.com>
2019-08-15 15:58:27 -0400

    modify map plot files

--- 01_Population_structure_and_cryptic_lineages/Structure_and_map_O_novo_ulmi/plotStructureMap.R
+++ 01_Population_structure_and_cryptic_lineages/Structure_and_map_O_novo_ulmi/plotStructureMap.R
@@ -1,21 +1,25 @@
 
-library(dplyr)
 source('Pipie.R', chdir = TRUE)
+library(dplyr)
+library(tidyr)
+library(plotrix)
+library(maps)
+library(mapdata)
+library(maptools)
 
-
-### Importing file containing the map
+### Importing file with borders
 fdc <- read.table("world_intermediate.txt", header = F)
 
 ### Importing file containing the coordinate of strains
-d <- read.table("Liste_finale_plaque1_LN2.txt", header = T)
-head(d)
-str(d)
+df <- read.table("samples_locations.txt", sep = "\t", header = TRUE, na.strings = "NA")
+s <- read.table("simulated_coords.txt", sep = "\t", header = TRUE, col.names = c("Long_sim", "Lat_sim", "lab"))
+m <- merge(s, df, by.x = "lab", by.y = "ID", sort = FALSE)
 
 ### Importing file containing the output of structure
 K3 <- read.table("k3.csv", sep = ",")
 head(K3)
 
-### A small function to handle more easily the color
+### Setting colors
 cl <- colors()
 goldenrod2 <- colors()[149]
 olivedrab4 <- colors()[497]
@@ -25,60 +29,66 @@ firebrick3 <- colors()[136]
 ### Plotting the map of Northern Hemispere
 
 
-#pdf("Figure_piechart_novoulmi.pdf", 8.82, 4.60)
+d <- m %>% filter(lab != '81' & lab != '82')
 
-plot(fdc, type = "l", col = "gray60", xlab = "Longitude", ylab = "Latitude", xlim = c(-140, 100), ylim = c(30, 70))
+pdf("Figure_piechart_novoulmi.pdf", 17, 6.5)
 
-# For each strain ...
+map("world",xlim = c(-140, 110), ylim = c(30, 70), fill = F, col = colors()[200], type = "l")
+map.axes(xaxt = "n", yaxt = "n")
 for(i in as.character(d$Isolate)){
-	# i="DDS100"
+  di <- filter(d, Isolate == i)
+  K3i <- filter(K3, V2 == i) %>% dplyr::select(-V1, -V2) %>% as.matrix
+  if (length(K3i) > 0) {
+    points(di$Longitude, di$Latitude, pch = 21, col = "black", bg = "black", cex = 0.4)
+    lines(x = c(di$Long_sim, di$Longitude), y = c(di$Lat_sim, di$Latitude), col="black")
+  }
+}
+for(i in as.character(d$Isolate)){
 	# Extract the strain to obtain its location
 	di <- filter(d, Isolate == i)
 	# Extract the strain to obtain its results in structure
-	K3i <- filter(K3, V2 == i) %>% select(-V1, -V2) %>% as.matrix
+	K3i <- filter(K3, V2 == i) %>% dplyr::select(-V1, -V2) %>% as.matrix
 	# If the strain is indeed in the stroucture output ...
 	if (length(K3i) > 0) {
-		# print a psmall black point at the sampling location
-		points(di$Longitude, di$Latitude, pch = 21, col = "black", bg = "black", cex = 0.4)
-		# print a line that will link the sampling location with the pie chart (structure results of the strain)
-		lines(x = c(di$Long_sim, di$Longitude), y = c(di$Lat_sim, di$Latitude), col="black")
-		# print the pie chart	
 		pipie(K3i, long = di$Long_sim, lat = di$Lat_sim, labels = "", radius = 0.05, 
 		      col = c(cl[136], cl[149], cl[497]),border=FALSE)
 	}
 }
-
-#dev.off()
+dev.off()
 
 
 ### Plotting the New Zealand
 
-#pdf("Figure_piechart_novoulmi_NZ.pdf", 2.6, 5.2)
+d <- m %>% filter(lab == '81' | lab == '82')
 
-# Plot the map
-plot(fdc, type = "l", col = "gray60", xlab = "", ylab = "", xlim = c(165, 180), ylim = c(-50, -25))
+pdf("Figure_piechart_novoulmi_NZ.pdf", 2.6, 5.2)
 
-# For each strains ...
-for(i in as.character(d$Isolate)){
-	#i="DDS100"
-	# Extract the strain to obtain its location	
-	di <- filter(d, Isolate == i)
-	# Extract the strain to obtain its results in structure
-	K3i <- filter(K3, V2 == i) %>% select(-V1, -V2) %>% as.matrix
+{
+  map("world",xlim = c(166, 181), ylim = c(-48, -31), fill = F, col = colors()[200], type = "l", bg = "white")
+  map.axes(xaxt = "n", yaxt = "n")
+  points(d$Longitude, d$Latitude, pch = 21, col = "black", bg = "black", cex = 0.4)
 
-	# If the strain is indeed in the stroucture output ...
-	if (length(K3i) > 0) {
-		# print a psmall black point at the sampling location
-		points(di$Longitude, di$Latitude, pch = 21, col = "black", bg = "black", cex = 0.4)
-		# print a line that will link the sampling location with the pie chart (structure results of the strain)
-		lines(x = c(di$Long_sim, di$Longitude), y = c(di$Lat_sim, di$Latitude), col="black")
-		# print the pie chart	
-		pipie(K3i, long = di$Long_sim, lat = di$Lat_sim, labels = "",
-		      radius = 0.09, col = c(cl[136], cl[149], cl[497]),border=FALSE)
-	}
+  # For each strains ...
+  for(i in as.character(d$Isolate)){
+  	# Extract the strain to obtain its location	
+  	di <- filter(d, Isolate == i)
+  	# Extract the strain to obtain its results in structure
+  	K3i <- filter(K3, V2 == i) %>% select(-V1, -V2) %>% as.matrix
+  
+  	# If the strain is indeed in the stroucture output ...
+  	if (length(K3i) > 0) {
+  		# print a psmall black point at the sampling location
+  		points(di$Longitude, di$Latitude, pch = 21, col = "black", bg = "black", cex = 0.4)
+  		# print a line that will link the sampling location with the pie chart (structure results of the strain)
+  		lines(x = c(di$Long_sim, di$Longitude), y = c(di$Lat_sim, di$Latitude), col="black")
+  		# print the pie chart	
+  		pipie(K3i, long = di$Long_sim, lat = di$Lat_sim, labels = "",
+  		      radius = 0.09, col = c(cl[136], cl[149], cl[497]),border=FALSE)
+  	}
+  }
 }
 
-#dev.off()
+dev.off()
 
 
 
